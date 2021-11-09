e6027b655e851592223237f43766cb72b3423381
Karol Skalik <celmaibun@o2.pl>
2021-11-09 21:46:23 +0100

    dfgdf

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-let loader = document.getElementById('preloader');
-
-window.addEventListener('load', function () {
-  loader.style.display = 'none';
-});
-
 const hamburger = document.getElementById('hamburger');
 const navUl = document.getElementById('navbar-mobile');
 const navItems = [...document.querySelectorAll('.navbar-mobile a')];
